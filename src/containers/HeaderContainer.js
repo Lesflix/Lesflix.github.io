0e069bdedda140f0c3d8da51fdb425bac995b8bb
0andme <yu00mi97@gmail.com>
2022-05-26 04:06:32 +0900

    style : 로고 스타일 수정

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -2,11 +2,14 @@ import styled from "@emotion/styled"
 import React from "react"
 import SearchInput from "../components/SearchInput/SearchInput"
 import HeaderNav from "../components/HeaderNav/HeaderNav"
+import { Link } from "gatsby"
 function HeaderContainer() {
   return (
     <StyleHeaderContainer>
       <div id="logo">
-        <img src={require("../images/letflix.svg").default} alt="logo" />
+        <Link to={`/`}>
+          <img src={require("../images/letflix.svg").default} alt="logo" />
+        </Link>
       </div>
       <HeaderNav />
       <SearchInput />
@@ -18,7 +21,6 @@ export default HeaderContainer
 const StyleHeaderContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   padding: 10px;
   position: fixed;
   top: 0;
@@ -27,8 +29,9 @@ const StyleHeaderContainer = styled.div`
   height: 10vh;
   border-bottom: 1px solid black;
   #logo {
-    height: calc(100% - 10px);
+    height: calc(10vh - 20px);
     img {
+      max-width: 30vw;
       height: 100%;
     }
   }
