502abfd4d4319d618a4297c33e0f0402bd319473
0andme <yu00mi97@gmail.com>
2022-05-26 05:56:21 +0900

    style : 헤더-장르카테고리-스타일 수정

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,4 +1,5 @@
-import { graphql, useStaticQuery } from "gatsby"
+import styled from "@emotion/styled"
+import { graphql, Link, useStaticQuery } from "gatsby"
 import React from "react"
 
 function HeaderNav() {
@@ -13,12 +14,51 @@ function HeaderNav() {
   `)
   const { group: navList } = data
   return (
-    <div>
+    <NavList>
       {navList?.map(data => (
-        <a href={`/${data.fieldValue}`}>{data.fieldValue}</a>
+        <Link to={`/${data.fieldValue}`} state={{ path: data.fieldValue }}>
+          <span>{data.fieldValue}</span>
+        </Link>
       ))}
-    </div>
+    </NavList>
   )
 }
 
 export default HeaderNav
+
+const NavList = styled.div`
+  display: flex;
+  margin: 0 10px;
+  flex: 1;
+  box-sizing: border-box;
+  gap: 10px;
+
+  span {
+    font-size: 2rem;
+    color: var(--Grey900);
+    font-weight: 700;
+
+    @media screen and (min-width: 320px) {
+      font-size: 2.2rem;
+    }
+    @media screen and (min-width: 375px) {
+      font-size: 2.4rem;
+    }
+    @media screen and (min-width: 475px) {
+      font-size: 2.6rem;
+    }
+    @media screen and (min-width: 768px) {
+      font-size: 3.5rem;
+    }
+    @media screen and (min-width: 1024px) {
+      font-size: 3.8rem;
+    }
+    @media screen and (min-width: 1440px) {
+      font-size: 4rem;
+    }
+  }
+  /* line-height: inherit; */
+  /* width: calc(100vw - 30% - 10px); */
+  /* background-color: red; */
+  /* gap: 10px; */
+`
