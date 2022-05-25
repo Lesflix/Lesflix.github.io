da257fa14b66b786bb32aaf93813e535d4470de7
0andme <yu00mi97@gmail.com>
2022-05-26 05:55:18 +0900

    feat : 장르별 데이터 가져오기

--- src/templates/genre.js
+++ src/templates/genre.js
@@ -1,8 +1,11 @@
+import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../components/layout"
 import HeaderContainer from "../containers/HeaderContainer"
 
-function Genre({ location }) {
+function Genre({ data, location }) {
+  const { path: genre } = location.state
+  const { nodes: PostList, totalCount } = data.allMarkdownRemark
   return (
     <Layout location={location}>
       <HeaderContainer />
@@ -10,3 +13,26 @@ function Genre({ location }) {
   )
 }
 export default Genre
+
+export const pageQuery = graphql`
+  query filteredPost($genre: String) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { genre: { eq: $genre } } }
+    ) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+          genre
+        }
+      }
+      totalCount
+    }
+  }
+`
