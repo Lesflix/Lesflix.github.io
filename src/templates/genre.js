e35c45073ea7d3d5d7e8070d1c2f7f4d86e3f716
0andme <yu00mi97@gmail.com>
2022-05-26 03:39:45 +0900

    feat : 장르 템플릿 추가

---
+++ src/templates/genre.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import HeaderContainer from "../containers/HeaderContainer"
+
+function Genre({ location }) {
+  return (
+    <Layout location={location}>
+      <HeaderContainer />
+    </Layout>
+  )
+}
+export default Genre
