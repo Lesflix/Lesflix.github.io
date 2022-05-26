6d009b5750f0b7efac83f488c4dda4ecda25b509
0andme <yu00mi97@gmail.com>
2022-05-26 22:48:42 +0900

    style : edit headerStyle

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -19,20 +19,53 @@ function HeaderContainer() {
 
 export default HeaderContainer
 const StyleHeaderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
+
+  display: flex;
+  align-items: center;
+
   height: 10vh;
   border-bottom: 1px solid black;
+
+  padding: 0 36px;
+
   #logo {
     height: calc(10vh - 20px);
+    margin-right: 17px;
+
     img {
-      max-width: 30vw;
       height: 100%;
+    }
+  }
+  @media screen and (min-width: 1920px) {
+    height: 172px;
+
+    #logo {
+      height: 100px;
+    }
+  }
+  @media screen and (min-width: 1920px) {
+    height: 172px;
+    #logo {
+      height: 100px;
+    }
+  }
+  @media screen and (min-width: 768px) {
+    height: 142px;
+    padding: 0 19px;
+    #logo {
+      height: 88px;
+    }
+  }
+  @media screen and (min-width: 375px) {
+    height: 72px;
+    padding: 0 12px;
+    #logo {
+      height: 47px;
+      margin-right: 10px;
     }
   }
 `
