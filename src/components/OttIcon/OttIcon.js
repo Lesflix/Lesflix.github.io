2fda3f413e414a56cd0e935ba3a666e7c2a0887f
0andme <yu00mi97@gmail.com>
2022-05-28 01:43:18 +0900

    feat : add ott list container

---
+++ src/components/OttIcon/OttIcon.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function OttIcon() {
+  return <div></div>
+}
+
+export default OttIcon
---
+++ src/containers/OttListContainer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function OttListContainer() {
+  return <div></div>
+}
+
+export default OttListContainer
