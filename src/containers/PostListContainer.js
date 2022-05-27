b4e78b997b4ef7a084fc000a0be8afc3337f3c22
0andme <yu00mi97@gmail.com>
2022-05-28 07:19:33 +0900

    style : edit style

--- src/containers/PostListContainer.js
+++ src/containers/PostListContainer.js
@@ -27,47 +27,37 @@ const PostListContainerStyle = styled.div`
     visibility: hidden;
   }
   @media screen and (min-width: 1920px) {
-    * {
-      width: 10%;
-    }
     .item {
       padding: 20px;
+      width: 10%;
     }
     margin: 30px -10px 0 0;
   }
   @media screen and (min-width: 1080px) and (max-width: 1919px) {
-    * {
-      width: 20%;
-    }
     .item {
       padding: 20px;
+      width: 20%;
     }
     margin: 25px -10px 0 0;
   }
   @media screen and (min-width: 768px) and (max-width: 1079px) {
-    * {
-      width: 25%;
-    }
     .item {
       padding: 16px;
+      width: 25%;
     }
     margin: 20px -8px 0 0;
   }
   @media screen and (min-width: 375px) and (max-width: 767px) {
-    * {
-      width: 33%;
-    }
     .item {
       padding: 8px;
+      width: 33%;
     }
     margin: 15px -4px 0 0;
   }
   @media screen and (max-width: 374px) {
-    * {
-      width: 50%;
-    }
     .item {
       padding: 6px;
+      width: 50%;
     }
     margin: 10px -3px 0 0;
   }
