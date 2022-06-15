06e9c328b8a80bf195506e124f96f1aa9f9a2df4
0andme <yu00mi97@gmail.com>
2022-06-16 00:20:06 +0900

    edit seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -29,7 +29,9 @@ const Seo = ({ description, lang, meta, title, imgName }) => {
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
-  const imgurl = imgName ? `/cover/${imgName}` : `/cover.png`
+  const imgurl = imgName
+    ? `https://lesflix.github.io/cover/${imgName}`
+    : `https://pbs.twimg.com/media/FVMDmpEUUAE8eUl?format=png&name=4096x4096`
   return (
     <Helmet
       htmlAttributes={{
